63000f7474b6d171273bc240b5c472abd7cf759d
Nathan <nathan@naykel.com.au>
2023-08-12 08:16:12 +1000

    initial commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# 3815 Team 30 Tetris
+
+Run development server:
+
+```bash
+npm run dev
+```
+
+Build project:
+
+```bash
+npm run build
+```
+
+Preview build project:
+
+```bash
+npm run preview
+```
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './styles.scss'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+    <div class="container h-screen flex ha-c">
+        <img src="/favicon.svg" alt="team 30 logo" class="w-10 h-auto">
+    </div>
+`
