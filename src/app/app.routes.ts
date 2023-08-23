4282aa9ab8c1b8790606da6d0ff3637efd10aeff
Nathan <nathan@naykel.com.au>
2023-08-23 16:53:26 +1000

    add pages and routing for better site structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path: '',
+    //     redirectTo: 'login',
+    //     pathMatch: 'full'
+    // },
+    {
+        path: 'start', loadComponent: () => import('./pages/start.component')
+            .then(mod => mod.StartComponent)
+    },
+    {
+        path: 'game', loadComponent: () => import('./pages/game.component')
+            .then(mod => mod.GameComponent)
+    },
+];
