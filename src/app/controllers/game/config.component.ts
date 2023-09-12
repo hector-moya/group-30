926f9e211c736a207737c2e7a5e1c17deabb60aa
Nathan <nathan@naykel.com.au>
2023-09-12 10:16:22 +1000

    remove tooltip component

--- src/app/controllers/game/config.component.ts
+++ src/app/controllers/game/config.component.ts
@@ -1,4 +1,3 @@
-import { ToolTipComponent } from '../components/tool-tip.component';
 import { IConfig } from 'src/app/interfaces/Config';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -7,7 +6,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-config',
     standalone: true,
-    imports: [CommonModule, FormsModule, ToolTipComponent],
+    imports: [CommonModule, FormsModule],
     templateUrl: '../../views/game/config.component.html',
     styles: []
 })
